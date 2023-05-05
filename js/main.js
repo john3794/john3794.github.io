7697f2634345bab6ff1f5cfe4b73315c0648e320
Johnny Ly <jolydeveloper@gmail.com>
2023-05-05 14:07:26 +0200

    Updated timer

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 31 2019 23:59:59",
+	date: "December 31 2024 23:59:59",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
